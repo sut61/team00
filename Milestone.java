61d3ce7c2032079e25e76b2c6320f3c4bea87592
chanwit <chanwit@gmail.com>
2018-12-03 04:16:23 +0000

    implement bug fix - close #6

--- Milestone.java
+++ Milestone.java
@@ -4,6 +4,7 @@ public class Milestone {
 
   public static void main(String[] args) throws Exception {
 	System.out.println("tag milestone to issue");
+        System.out.println("implement feature 6");
   }
 
 }
