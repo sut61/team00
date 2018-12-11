13fdf078ecd32c499516934b304efe385522db01
virusman1 <xirb_01@hotmail.co.th>
2018-12-11 16:39:10 +0700

    make file test48 for issue 16-close#16

---
+++ test48.java
@@ -0,0 +1,10 @@
+﻿package main;
+
+public class test48 {
+
+  public static void main(String[] args) throws Exception {
+	System.out.println("issue 16");
+        System.out.println("But Sweat16! is not 48 group ");
+  }
+
+}
